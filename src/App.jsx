98a5990515ed3f56920cc1a38cbd3584af987f71
Sushant Bahirat <sushantbahirat40@gmail.com>
2022-07-16 17:11:30 +0530

    initial commit with basic skeleton

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+
+const App = () => {
+	return (
+		<Box width={"400px"} sx={{ width: { xl: "1488px" } }} m="auto">
+			<Navbar />
+
+			<Routes>
+				<Route path={"/"} element={<Home />} />
+				<Route path={"/exercise/:id"} element={<ExerciseDetail />} />
+			</Routes>
+
+			<Footer />
+		</Box>
+	);
+};
+
+export default App;

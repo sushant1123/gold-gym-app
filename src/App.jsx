8269f95672c57d26f31afa3ced4f7aaaecb7f9bd
Sushant Bahirat <sushantbahirat40@gmail.com>
2022-07-16 17:16:30 +0530

    updated commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { Box } from "@mui/material";
 import Home from "./pages/Home";

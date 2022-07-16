a5eda76fad76c2ec2dbee8654e8508f7332cba57
Sushant Bahirat <sushantbahirat40@gmail.com>
2022-07-16 22:07:08 +0530

    add homepage layout.

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImg from "../assets/images/banner.png";
+
+const HeroBanner = () => {
+	return (
+		<Box sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }} position="relative" p="20px">
+			<Typography color="#ff2625" fontSize="26px" fontWeight="600">
+				Fitness Club
+			</Typography>
+			<Typography
+				fontSize="52px"
+				fontWeight="700"
+				sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+				mb="23px"
+				mt="30px"
+			>
+				Sweat, Smile <br /> and Repeat
+			</Typography>
+			<Typography fontSize="22px" lineHeight="35px" mb={4}>
+				Checkout the most effective exercises
+			</Typography>
+			<Button
+				variant="contained"
+				color="error"
+				href="#exercises"
+				sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+			>
+				Explore Exercises
+			</Button>
+			<Typography
+				color="#ff2625"
+				sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+				fontWeight="600"
+				fontSize="200px"
+			>
+				Exercise
+			</Typography>
+			<img src={HeroBannerImg} alt="banner" className="hero-banner-img" />
+		</Box>
+	);
+};
+
+export default HeroBanner;
